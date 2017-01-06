d948f590d909eb4b680ac9aa510d181490de5082
pierredi <repio2000@hotmail.com>
2017-01-06 13:31:30 +0000

    MAj librairie des couleurs ajout méthode écriture, ajout couleur

--- colorlib.py
+++ colorlib.py
@@ -6,7 +6,6 @@ ENCRE_JAUNE_VIOLET = [str(1), str(33), str(45)]
 ENCRE_BLEU_VIOLET = [str(1), str(36), str(45)]
 ENCRE_BLEU_BLANC = [str(4), str(34), str(47)]
 ENCRE_BLANC_BLEU = [str(1), str(37), str(44)]
-ENCRE_BLANC_ORANGE = [str(1), str(37), str(43)]
 ENCRE_VERT_BLANC = [str(5), str(32), str(47)]
 ENCRE_BLANC_VERT = [str(7), str(32), str(47)]
 ENCRE_BLANC = [str(1), str(37), str(40)]
@@ -17,6 +16,14 @@ ENCRE_BLANC_ROUGE_U = [str(4), str(31), str(47)]
 ENCRE_ROUGE_BLANC = [str(7), str(31), str(47)]
 ENCRE_NANTES = [str(1), str(33), str(42)]
 ENCRE_LORIENT = [str(7), str(30), str(43)]
+RED = u"\033[91m"
+GREEN = u"\033[1;92m"
+YELLOW = u"\033[1;93m"
+LIGHT_PURPLE = u"\033[94m"
+PURPLE = u"\033[1;95m"
+CLIGNOTE = u"\033[25m"
+END = u"\033[0m"
+BLINK = u"\033[5m"
 
 
 def liste_couleur():
@@ -38,5 +45,41 @@ def ecrire(couleur, phrase):
     return "\x1b[%sm%s\x1b[0m" % (tableau_couleur, phrase)
 
 
+def clignote(name):
+    result = CLIGNOTE+u" {}"+END
+    return str(result .format(name))
+
+
+def violet(name):
+    result = PURPLE+u" {}"+END
+    return str(result .format(name))
+
+
+def violet_clair(name):
+    result = LIGHT_PURPLE+u" {}"+END
+    return str(result .format(name))
+
+
+def rouge(name):
+    result = RED+u" {}"+END
+    return str(result .format(name))
+
+
+def vert(name):
+    result = GREEN+u" {}"+END
+    return str(result .format(name))
+
+
+def jaune(name):
+    result = YELLOW+u" {}"+END
+    return str(result .format(name))
+
+
 if __name__ == "__main__":
     liste_couleur()
+    print violet("test ecriture violet")
+    print violet_clair("test ecriture violet clair")
+    print rouge("test ecriture rouge")
+    print vert("test ecriture vert")
+    print jaune("test ecriture jaune")
+    print clignote("test ecriture jaune")
